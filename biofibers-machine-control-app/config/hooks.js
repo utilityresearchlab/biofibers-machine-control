28c14857f24fdb9c18a4eeb609147f448ea6ae26
Michael Rivera <mrivera.lee@gmail.com>
2024-05-31 20:12:11 -0600

    add hooks, fs-extra, and dist/package commands, with app icon

---
+++ biofibers-machine-control-app/config/hooks.js
@@ -0,0 +1,42 @@
+const fs = require('fs-extra');
+const path = require('path');
+const spawn = require('child_process').spawn;
+
+module.exports = {
+    postPackage: async (forgeConfig, options) => {
+        console.warn('\n\npostPackage: exclude files ...\n\n');
+    },
+    readPackageJson: async (forgeConfig, packageJson) => {
+    // only copy deps if there isn't any
+    if (Object.keys(packageJson.dependencies).length === 0) {
+        const originalPackageJson = await fs.readJson(path.resolve("../", 'package.json'));
+        const webpackConfigJs = require('./webpack.renderer.config.js');
+        Object.keys(webpackConfigJs.externals).forEach(package => {
+        packageJson.dependencies[package] = originalPackageJson.dependencies[package];
+        });
+    }
+    return packageJson;
+    },
+    packageAfterPrune: async (forgeConfig, buildPath) => {
+    console.log(buildPath);
+    return new Promise((resolve, reject) => {
+        const npmInstall = spawn('npm', ['install'], {
+        cwd: buildPath,
+        stdio: 'inherit',
+        shell: true
+        });
+
+        npmInstall.on('close', (code) => {
+        if (code === 0) {
+            resolve();
+        } else {
+            reject(new Error('process finished with error code ' + code));
+        }
+        });
+
+        npmInstall.on('error', (error) => {
+        reject(error);
+        });
+    });
+    }
+};
