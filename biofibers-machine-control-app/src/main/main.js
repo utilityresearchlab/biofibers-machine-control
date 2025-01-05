a8294ecfeb4834dcb20cf34bb303fd898ce41e16
Michael Rivera <mrivera.lee@gmail.com>
2025-01-04 19:13:55 -0700

    Force opening target='_blank' urls in browser window

--- biofibers-machine-control-app/src/main/main.js
+++ biofibers-machine-control-app/src/main/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, shell } = require('electron');
 const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -21,6 +21,12 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
+  // Open '_target' blank urls in browser
+  mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
+
   // Open the DevTools if in debugging mode
   if (!app.isPackaged) {
     mainWindow.webContents.openDevTools();
@@ -31,6 +37,8 @@ const createWindow = () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
+
+
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -48,6 +56,7 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
