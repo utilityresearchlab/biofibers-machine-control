f6736f8a4e97f9a39f881fe8c7f6f194c76707c8
Michael Rivera <mrivera.lee@gmail.com>
2025-01-08 17:16:23 -0700

    add millis to time lib output

--- biofibers-machine-control-app/src/renderer/lib/time.js
+++ biofibers-machine-control-app/src/renderer/lib/time.js
@@ -1,10 +1,14 @@
 
-function getReadableTimeOfDay(timestamp) {
+function getReadableTimeOfDay(timestamp, includeMillis=false) {
 	const dt = new Date(timestamp);
 	const hours = String(dt.getHours()).padStart(2, '0');
 	const minutes = String(dt.getMinutes()).padStart(2, '0');
 	const seconds = String(dt.getSeconds()).padStart(2, '0');
- 	const timeString = `${hours}:${minutes}:${seconds}`;
+	const timeString = `${hours}:${minutes}:${seconds}`;
+	if (includeMillis) {
+		const ms = String(dt.getMilliseconds()).padEnd(3, '0');
+		return `${timeString}.${ms}` 
+	}
 	return timeString;
 }
 
