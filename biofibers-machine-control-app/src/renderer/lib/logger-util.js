29e3cd9fc00c4030e9fea850443ce43974404f82
Michael Rivera <mrivera.lee@gmail.com>
2025-01-09 08:24:32 -0700

    add logger util

---
+++ biofibers-machine-control-app/src/renderer/lib/logger-util.js
@@ -0,0 +1,25 @@
+import * as APP_SETTINGS from '../app-settings'
+
+const LOG_TAG_DELIMITER = ":";
+
+export function logD(...info) {
+    if (APP_SETTINGS.DEBUG_MODE) {
+	    console.log(`DEBUG${LOG_TAG_DELIMITER}`, ...info);
+    }
+};
+
+export function logE(...info) {
+	console.error(`ERROR${LOG_TAG_DELIMITER}`, ...info);
+};
+
+export function logW(...info) {
+	console.warn(`WARNING${LOG_TAG_DELIMITER}`, ...info);
+};
+
+export function logI(...info) {
+	console.info(`INFO${LOG_TAG_DELIMITER}`, ...info);
+};
+
+export function log(...info) {
+	console.log(`App${LOG_TAG_DELIMITER}`, ...info);
+};
