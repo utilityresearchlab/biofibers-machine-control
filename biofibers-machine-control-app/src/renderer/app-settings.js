20a6d753a6ee8874569cd4a8492d2f1cb43af652
Michael Rivera <mrivera.lee@gmail.com>
2025-01-09 08:24:59 -0700

    add debug flag to app settings

--- biofibers-machine-control-app/src/renderer/app-settings.js
+++ biofibers-machine-control-app/src/renderer/app-settings.js
@@ -2,3 +2,6 @@
 
 export const UTILITY_RESEARCH_LAB_URL = "https://utilityresearchlab.org";
 export const BIOFIBERS_GITHUB_URL = "https://github.com/utilityresearchlab/desktop-biofibers-spinning";
+
+// Toggles logging for debug mode
+export const DEBUG_MODE = true;
