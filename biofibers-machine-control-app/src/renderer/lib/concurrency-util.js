7a63ac171db70d23a4705cd17e9891c9f491d6ba
Michael Rivera <mrivera.lee@gmail.com>
2025-05-20 17:18:24 -0600

    Add a concurrent counter for tracking NACK lines #5

---
+++ biofibers-machine-control-app/src/renderer/lib/concurrency-util.js
@@ -0,0 +1,32 @@
+class SafeCounter {
+	constructor() {
+	  this.count = 0;
+	  this.queue = Promise.resolve();
+	}
+  
+	increment(by = 1) {
+	  this.queue = this.queue.then(() => {
+		return new Promise((resolve) => {
+		  this.count += by;
+		  console.log(`Counter updated: ${this.count}`);
+		  resolve();
+		});
+	  });
+	}
+
+	decrement(by = 1) {
+		this.queue = this.queue.then(() => {
+		  return new Promise((resolve) => {
+			this.count -= by;
+			console.log(`Counter updated: ${this.count}`);
+			resolve();
+		  });
+		});
+	  }
+  
+	get value() {
+	  return this.count;
+	}
+  }
+  
+export {SafeCounter};
