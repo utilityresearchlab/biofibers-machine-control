66641f832f5372d91ee163bf98b1db0ccd52b7f1
Michael Rivera <mrivera.lee@gmail.com>
2025-05-23 10:09:57 -0600

    add feature flags to app settings #12 #16

--- biofibers-machine-control-app/src/renderer/app-settings.js
+++ biofibers-machine-control-app/src/renderer/app-settings.js
@@ -1,5 +1,4 @@
-
-
+// These are app wide settings
 export const UTILITY_RESEARCH_LAB_URL = "https://utilityresearchlab.org";
 export const BIOFIBERS_GITHUB_URL = "https://github.com/utilityresearchlab/desktop-biofibers-spinning";
 
@@ -7,3 +6,7 @@ export const BIOFIBERS_GITHUB_URL = "https://github.com/utilityresearchlab/deskt
 export const DEBUG_MODE = true;
 
 export const MACHINE_INIT_TIMEOUT = 2000;
+
+// Feature Flags to hide in-development features
+export const FEATURE_FLAGS = {};
+FEATURE_FLAGS.SHOW_MATERIAL_SELECT_PULL_DOWN = false;
